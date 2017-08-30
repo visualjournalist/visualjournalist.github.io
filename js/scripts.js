ad26e3be09fc25b973d79c17fb496e540caf0bc7
Brian <visualjournalist@gmail.com>
2017-08-29 21:23:54 -0400

    Adding basic toggle for dropdown nav.

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+//Grab the spreadsheet KEY from the URL bar (NOT from the published window)
+var public_spreadsheet_url = '1LHNJt8AXc6xLV1d8sX9ys15JA2UxWOeINYsHHXidcqk';
+
+
+
+var debugMode = true;
+// Basic function to replace console.log() statements so they can all be disabled as needed;
+function logger(logString){
+	if (debugMode){
+		console.log(logString);
+	}
+}
+
+
+
+
+
+
+
+
+// ============================
+// |  Basic tabletopJS setup  |
+// ============================
+function loadSpreadsheet() {
+	if ( mode == "editing") {
+		Tabletop.init( { key: public_spreadsheet_url,
+			callback: showInfo,
+			simpleSheet: true } )
+		/*
+		//multisheet version: 
+		Tabletop.init( { key: public_spreadsheet_url,
+		 	callback: showInfo,
+		 	wanted: [ "religion", "parties" ] } )
+		*/
+	} else if ( mode == "production") {
+		//buildPresidents(d3target);
+		showInfo(bakedData);
+	} else {
+		console.log("You need to define the 'mode' ('editing' or 'production')");
+	}
+}
+//function showInfo(data, tabletop) {
+function showInfo(data) {
+	logger("loaded spreadsheet data: ");
+	logger(data);
+}
+
+
+
+$(document).ready(function(){
+	logger("Ready");
+
+	// ===================
+	// |  Dropdown menu  |
+	// ===================
+	$(function() {
+		$('#main-menu').smartmenus({
+			subMenusSubOffsetX: 1,
+			subMenusSubOffsetY: -8
+		});
+	});
+
+	$( "#menuButton" ).click(function() {
+		logger("clicked menu toggle")
+	  $( ".main-menu-nav").toggle();
+	});
+
+	$( ".main-menu-nav a").not(".has-submenu").click(function() {
+	  $( ".main-menu-nav").hide();
+	});
+	/*
+	$( ".voa__section__full-width" ).click(function() {
+	  $( ".main-menu-nav").hide();
+	});
+	*/
+
+
+
+
+	/*
+	// ================================================
+	// |  Opens a pop-up with twitter sharing dialog  |
+	// ================================================
+	$('#shareTwitter').click(function(){
+		var url = "http://projects.voanews.com";
+		var text = "Replace this with your text";
+		window.open('http://twitter.com/share?url='+encodeURIComponent(url)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+	})
+	*/
+
+
+});
